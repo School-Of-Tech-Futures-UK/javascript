738ad4fe9cfdb8c42a88a93934151b04320ffb58
ankie17 <anutik26785@gmail.com>
2022-01-12 16:46:16 +0000

    started reset button

--- Connect4game/connect4.js
+++ Connect4game/connect4.js
@@ -2,7 +2,6 @@ const tag = document.getElementById("header")
 tag.innerText = "connect 4"
 let turn = 0
 let player1 = "red"
-
 let grid = [
     [null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null],
@@ -15,16 +14,13 @@ let grid = [
 function takeTurn(e) {
     const id = e.target.id   // 'row1-col1'   ________x
     // 'rowY-colX' 
-
     const colNum = id[8]
     const rowNum = id[3]
-
     const lowestAvailableRow = getLowestAvailableRowInColumn(colNum, grid)
     console.log(`Lowest available row: ${lowestAvailableRow}`)
 
     if (lowestAvailableRow !== null) {
         turn++
-
         if (player1 === "red") {
             grid[lowestAvailableRow][colNum - 1] = "red"
             document.getElementById(`row${lowestAvailableRow + 1}-col${colNum}`).style.backgroundColor = 'red';
@@ -35,24 +31,29 @@ function takeTurn(e) {
             player1 = "red"
         }
     }
-
-
-
-
-
     console.log(`You clicked column ${colNum}`)
     console.log(`Turn number ${turn}`)
-    console.log(grid)
+    //console.log(grid)
+}
+
+function Reset(e) {
+    grid = [
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null]
+    ]
+    console.log('You clicked reset')
 }
 
 
-function getLowestAvailableRowInColumn(cynthiaColumnNumber, myGridSoItIs) {
+function getLowestAvailableRowInColumn(ColumnNumber, myGridSoItIs) {
     for (let i = 5; i >= 0; i--) {
-        if (myGridSoItIs[i][cynthiaColumnNumber - 1] === null) {
+        if (myGridSoItIs[i][ColumnNumber - 1] === null) {
             return i
         }
     }
-
-
     return null;
 }
